af4ae78595764ac312ae9b71b637975f43a856df
jnicewander <jnicewander@live.com>
2020-11-14 16:33:14 -0500

    Added data to show form live updates in browser.

--- src/components/form.js
+++ src/components/form.js
@@ -1,6 +1,16 @@
 import React, { useReducer, useState } from 'react'
+import styled from 'styled-components'
+
 import { FormReducer, DefaultForm } from '../utils/form-reducer'
 import FormSteps from './form-steps'
+import { textInputData, checkboxData, radioGroupData } from '../utils/data-source'
+import { check } from 'prettier'
+
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
+
 
 const Form = () => {
   let [step, setStep] = useState(0)
@@ -9,10 +19,31 @@ const Form = () => {
   console.log(form)
   
   return (
-    <div>
-      <h2>{steps[`${step}`].title} - Step {step + 1}/{steps.length}</h2>
-      <div>{steps[`${step}`].content}</div>
-    </div>
+    <Container>
+      <div>
+        <h2>{steps[`${step}`].title} - Step {step + 1}/{steps.length}</h2>
+        <div>{steps[`${step}`].content}</div>
+      </div>
+      <div>
+        <h3>Current Form Entries</h3>
+        <ul>
+          {/* Why can't I destructure inside the map callback? */}
+          {textInputData.map(dataObject => (
+            <li key={dataObject.name}>
+              {dataObject.label}: {form[dataObject.name]}
+            </li>
+          ))}
+          {checkboxData.map(dataObject => (
+            <li key={dataObject.name}>
+              {dataObject.value}: 
+              {!form[dataObject.name] || form[dataObject.name].length === 0 
+                ? '' 
+                : 'Checked'}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </Container>
   )
 }
 
