08034b48652580ff5c6fc104c735c11475d59686
jnicewander <jnicewander@live.com>
2020-11-29 12:41:33 -0500

    Progress bar is functional without using icons.

--- src/components/progress-bar.js
+++ src/components/progress-bar.js
@@ -1,23 +1,55 @@
 import React from 'react'
 import styled from 'styled-components'
-import GhostIcon from '../common/ghost-icon'
 
 const FlexRow = styled.div`
   display: flex;
   width: 100%;
   justify-content: center;
+  align-items: center;
   margin: 2rem 0;
+`
+const Divider = styled.div`
+  width: 50px;
+  height: 5px;
+  border-radius: 10px;
+  background-color: ${ props => props.fill };
+  transition: background-color 1.4s ease-out;
+`
+const StepMarker = styled.div`
+  border-radius: 50%;
+  width: ${ props => props.size };
+  height: ${ props => props.size };
+  border: ${ props => props.border };
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: ${ props => props.background };
+  &::before {
+    content: '${ props => props.inner }';
+  }
 `
 const ProgressBar = ({ steps, stepNumber }) => {
   return (
-        steps.map((currentStep, index) => (
-          <GhostIcon
-            key={index}
-            fill={ stepNumber >= index ? 'blue' : 'black' }
-            height='2.5rem'
-            width='2.5rem'
-          />
-        ))
+    <FlexRow>
+        {
+          steps.slice(0, -1).map((currentStep, index) => (
+            <>
+              {
+                index > 0 &&
+                <Divider 
+                  fill={ stepNumber >= index ? 'lightblue' : 'lightgrey' }
+                />
+              }
+              <StepMarker
+                inner={ stepNumber > index ? '\\2713' : index + 1 }
+                border={ stepNumber > index ? 'none' : '2px solid lightgrey' }
+                background={ stepNumber > index ? 'lightblue' : 'none' }
+                size={ stepNumber === index ? '60px' : '40px' }
+              />
+            </>
+          ))
+        }
+    </FlexRow>
   )}
 
 export default ProgressBar
